19b760a820bafdc4a039a2d7e97f10a72c610a99
zhangchaoshuai <zsc451283@163.com>
2019-09-02 21:10:59 +0800

    feat: leetcode-两数之和/相加

---
+++ src/com/leetcode/leetcodes/leetcode/editor/cn/[1]两数之和.java
@@ -0,0 +1,94 @@
+package com.leetcode.leetcodes.leetcode.editor.cn;
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为
+// 目标值的那 两个 整数，并返回他们的数组下标。
+//
+// 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。 
+//
+// 示例: 
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+// 
+// Related Topics 数组 哈希表
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    public static void main(String[] args) {
+        int[] ints = new Solution().twoSum3(new int[]{2, 5, 5, 11}, 10);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i]);
+        }
+    }
+
+    /**
+     * 两个for循环
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            //key是和自己相匹配的数值，value是自己的值的下标
+            map.put(target - nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            //key是自己和自己相匹配的，value是自己的值
+            if (map.containsKey(nums[i]) && map.get(nums[i]) != i) {
+                return new int[]{i, map.get(nums[i])};
+            }
+        }
+        return new int[]{};
+    }
+
+    /**
+     * 可以再第一个for循环里就开始进行判断
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            //key是和自己相匹配的数值，value是自己的值的下标
+            map.put(target - nums[i], i);
+            //后面的判断是防止自己+自己=目标值
+            if (map.containsKey(nums[i]) && map.get(nums[i]) != i) {
+                return new int[]{i, map.get(nums[i])};
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 这种方法一定要记住，不能忘
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum3(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/com/leetcode/leetcodes/leetcode/editor/cn/[2]两数相加.java
@@ -0,0 +1,137 @@
+package com.leetcode.leetcodes.leetcode.editor.cn;
+//给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按
+// 照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+//
+// 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。 
+//
+// 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+//
+// 示例： 
+//
+// 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+//输出：7 -> 0 -> 8
+//原因：342 + 465 = 807
+// 
+// Related Topics 链表 数学
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution2 {
+
+    public static void main(String[] args) {
+        Solution2 solution2 = new Solution2();
+        ListNode n1 = new ListNode(5);
+        n1.next = new ListNode(4);
+        n1.next.next = new ListNode(3);
+        ListNode n2 = new ListNode(5);
+        n2.next = new ListNode(6);
+        n2.next.next = new ListNode(4);
+        n2.next.next.next = new ListNode(7);
+        ListNode listNode = solution2.addTwoNumbers3(n1, n2);
+        while (listNode != null) {
+            System.out.print(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int jinwei = 0;
+        ListNode listNode = new ListNode(0);
+        ListNode tmp = listNode;
+        while (l1 != null || l2 != null || jinwei != 0) {
+            int num = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + jinwei;
+            if (num > 9) {
+                jinwei = 1;
+            } else {
+                jinwei = 0;
+            }
+            tmp.val = num % 10;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+            //只要有一个还没遍历完成，就再构造一个next节点
+            //之前想着一个完成以后，直接把尾部指向未遍历完成的那部
+            // 分就行，但这是不行的，因为如果有进位1，并且未完成部分是999，则不能直接指向
+            if (l1 != null || l2 != null || jinwei != 0) {
+                tmp.next = new ListNode(0);
+            }
+            tmp = tmp.next;
+        }
+        return listNode;
+    }
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode(0);
+        ListNode tmp = listNode;
+        while (l1 != null || l2 != null) {
+            int num = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + (tmp == null ? 0 : tmp.val);
+            tmp.val = num % 10;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+            //只要有一个还没遍历完成，就再构造一个next节点
+            //之前想着一个完成以后，直接把尾部指向未遍历完成的那部
+            // 分就行，但这是不行的，因为如果有进位1，并且未完成部分是999，则不能直接指向
+            if (num > 9) {
+                tmp.next = new ListNode(1);
+            } else {
+                tmp.next = new ListNode(0);
+            }
+            if (l1 == l2) {
+                if (tmp.next.val == 0) {
+                    tmp.next = null;
+                }
+            } else {
+                tmp = tmp.next;
+            }
+
+        }
+        return listNode;
+    }
+
+    public ListNode addTwoNumbers3(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode(0);
+        ListNode tmp = listNode;
+        while (l1 != l2) {
+            int num = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + (tmp == null ? 0 : tmp.val);
+            tmp.val = num % 10;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+            //只要有一个还没遍历完成，就再构造一个next节点
+            //之前想着一个完成以后，直接把尾部指向未遍历完成的那部
+            // 分就行，但这是不行的，因为如果有进位1，并且未完成部分是999，则不能直接指向
+
+            //这里只有l1==null l2==null时才会成立
+            if (l1 == l2) {
+                if (num / 10 != 0) {
+                    tmp.next = new ListNode(num / 10);
+                }
+            } else {
+                tmp.next = new ListNode(num / 10);
+                tmp = tmp.next;
+            }
+
+        }
+        return listNode;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
